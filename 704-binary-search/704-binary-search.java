6688af12f0d86602d57d5538e350127b30d41e3d
Anish Ranjan <34716459+Anish51199@users.noreply.github.com>
2022-03-26 22:28:01 +0530

    Time: 1 ms (15.82%), Space: 54.1 MB (62.34%) - LeetHub

---
+++ 704-binary-search/704-binary-search.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int search(int[] arr, int x) {
+        int l = 0, r = arr.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+ 
+            // Check if x is present at mid
+            if (arr[m] == x)
+                return m;
+ 
+            // If x greater, ignore left half
+            if (arr[m] < x)
+                l = m + 1;
+ 
+            // If x is smaller, ignore right half
+            else
+                r = m - 1;
+        }
+ 
+        return -1;
+            
+    }
+}
